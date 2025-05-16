450c4a3a46c31ce1b87f334ebb1cd3053bb7eb06
JC8267 <34658858+JC8267@users.noreply.github.com>
2025-05-16 15:54:46 -0400

    Delete helpers.py

--- helpers.py
+++
@@ -1,8 +0,0 @@
-
-COMPETITOR_MAP = {
-    "amazon":   "amazon.com",
-    "target":   "target.com",
-    "wal-mart": "walmart.com",       # note the hyphen in your sheet
-    "walmart":  "walmart.com",
-    "babylist": "babylist.com",
-}
